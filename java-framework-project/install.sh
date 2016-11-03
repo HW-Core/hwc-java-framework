c535cb06705b08c2bc2418b3c5a659d3c47cbd41
Yehonal <yehonal.azeroth@gmail.com>
2016-11-03 11:15:08 +0100

    Updated

--- java-framework-project/install.sh
+++ java-framework-project/install.sh
@@ -4,11 +4,15 @@ PATH_MODULES="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../../"
 [ ! -d $PATH_MODULES/drassil/joiner ] && git clone https://github.com/drassil/joiner $PATH_MODULES/drassil/joiner -b master
 source "$PATH_MODULES/drassil/joiner/joiner.sh"
 
-NAME="java-lib-climenu"
 VENDOR="hw-core"
 
 #
 # ADD DEPENDENCIES
 #
 
+
 Joiner:add_repo "https://github.com/HW-Core/java-lib-common.git"      "java-lib-common"          "master" "$VENDOR"
+Joiner:add_repo "https://github.com/HW-Core/java-lib-climenu.git"      "java-lib-climenu"          "master" "$VENDOR"
+Joiner:add_repo "https://github.com/HW-Core/java-lib-database.git"      "java-lib-database"          "master" "$VENDOR"
+Joiner:add_repo "https://github.com/HW-Core/java-lib-xml.git"      "java-lib-xml"          "master" "$VENDOR"
+
--- java-lib-common/install.sh
+++ java-lib-common/install.sh
@@ -4,11 +4,15 @@ PATH_MODULES="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../../"
 [ ! -d $PATH_MODULES/drassil/joiner ] && git clone https://github.com/drassil/joiner $PATH_MODULES/drassil/joiner -b master
 source "$PATH_MODULES/drassil/joiner/joiner.sh"
 
-NAME="java-lib-climenu"
+NAME="java-lib-common"
 VENDOR="hw-core"
+
+mod_path="hw-core/js-modules/"
 
 #
 # ADD DEPENDENCIES
 #
 
-Joiner:add_repo "https://github.com/HW-Core/java-lib-common.git"      "java-lib-common"          "master" "$VENDOR"
+Joiner:add_file --unzip "http://www.java2s.com/Code/JarDownload/javax.servlet/javax.servlet-3.0.jar.zip"      "$mod_path/javax/servlet/javax.servlet-3.0.jar.zip" 
+
+
